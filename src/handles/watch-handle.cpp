343dcbe2057c9083aa508577912b990723a72fe8
weijia yuan <weijia@cs.ucla.edu>
2018-03-11 20:43:47 -0700

    Fix codestyle
    Change-Id: I2983ffdc44f7899ddc726432e82667d7f92d400d

--- src/handles/watch-handle.cpp
+++ src/handles/watch-handle.cpp
@@ -1,6 +1,6 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /*
- * Copyright (c) 2014-2017, Regents of the University of California.
+ * Copyright (c) 2014-2018, Regents of the University of California.
  *
  * This file is part of NDN repo-ng (Next generation of NDN repository).
  * See AUTHORS.md for complete list of repo-ng authors and contributors.
@@ -151,7 +151,6 @@ WatchHandle::onTimeout(const ndn::Interest& interest, const Name& name)
   }
   if (!onRunning(name))
     return;
-  
   Interest fetchInterest(interest.getName());
   fetchInterest.setInterestLifetime(m_interestLifetime);
 
@@ -291,7 +290,6 @@ WatchHandle::processWatchCommand(const Interest& interest,
   m_processes[parameter.getName()] =
                 std::make_pair(RepoCommandResponse().setStatusCode(300), true);
   Interest fetchInterest(parameter.getName());
-  
   fetchInterest.setInterestLifetime(m_interestLifetime);
   m_startTime = steady_clock::now();
   m_interestNum++;
--- src/handles/write-handle.cpp
+++ src/handles/write-handle.cpp
@@ -1,6 +1,6 @@
 /* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
 /*
- * Copyright (c) 2014-2017, Regents of the University of California.
+ * Copyright (c) 2014-2018, Regents of the University of California.
  *
  * This file is part of NDN repo-ng (Next generation of NDN repository).
  * See AUTHORS.md for complete list of repo-ng authors and contributors.
@@ -452,7 +452,6 @@ WriteHandle::processSingleInsertCommand(const Interest& interest,
 
   Interest fetchInterest(parameter.getName());
   fetchInterest.setInterestLifetime(m_interestLifetime);
-  
   getFace().expressInterest(fetchInterest,
                             bind(&WriteHandle::onData, this, _1, _2, processId),
                             bind(&WriteHandle::onTimeout, this, _1, processId), // Nack
